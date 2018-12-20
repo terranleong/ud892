75b8579d1678e79bd6fe7bdb35d3bf1a8987db6e
terran <terranleong@gmail.com>
2018-12-20 19:43:17 +0800

    Modified gulpfile.js

--- Lesson 2/gulpfile.js
+++ Lesson 2/gulpfile.js
@@ -1,3 +1,5 @@
+/*eslint-disable */
+
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
@@ -5,6 +7,8 @@ const browserSync = require("browser-sync").create();
 const postcss = require("gulp-postcss");
 const cssnano = require("cssnano");
 const sourcemaps = require("gulp-sourcemaps");
+const eslint = require('gulp-eslint');
+const jasmineBrowser = require('gulp-jasmine-browser');
 
 function reload() {
   browserSync.reload();
@@ -17,6 +21,12 @@ var paths = {
       src: "sass/**/*.scss",
       
       dest: "./css"
+  },
+
+  lint:{
+
+    src: "js/**/*.js"
+
   }
 
 };
@@ -60,6 +70,37 @@ function style() {
 
 exports.style = style;
 
+function lint(){
+
+  return (
+    gulp
+      .src([paths.lint.src, lint])
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failOnError())
+  );
+
+}
+
+exports.lint = lint
+
+// gulp.task('lint', function() {
+//   return (
+//       gulp
+//           .src(['js/**/*.js'])
+//           // eslint() attaches the lint output to the eslint property
+//           // of the file object so it can be used by other modules.
+//           .pipe(eslint())
+//           // eslint.format() outputs the lint results to the console.
+//           // Alternatively use eslint.formatEach() (see Docs).
+//           .pipe(eslint.format())
+//           // To have the process exit with an error code (1) on
+//           // lint error, return the stream and pipe to failOnError last.
+//           .pipe(eslint.failOnError())
+//   );
+// });
+
+
 // gulp.task("default", gulp.series("style", function() {
 
 //   gulp.watch("sass/**/*.scss", ["style"]);
@@ -75,6 +116,7 @@ function watch(){
   style();
 
   gulp.watch(paths.style.src, style);
+  gulp.watch(paths.lint.src, lint);
   gulp.watch("./index.html", reload);
 
   browserSync.init({
@@ -84,7 +126,34 @@ function watch(){
   
 exports.watch = watch
 
+// function test(){
 
+//     return gulp
+//     .src('tests/spec/extraSpec.js')
+//     .pipe(jasmineBrowser.specRunner({ console: true }))
+//     .pipe(jasmineBrowser.headless({ driver: 'chrome' }));
+
+// }
+
+gulp.task('tests', function() {
+  return gulp
+      .src(['src/**/*.js', 'spec/**/*_spec.js'])
+      .pipe(jasmineBrowser.specRunner({ console: true }))
+      .pipe(jasmineBrowser.headless({ driver: 'chrome' }));
+});
+
+function test(){
+
+  gulp
+  .src(['src/**/*.js', 'spec/**/*_spec.js'])
+  .pipe(jasmineBrowser.specRunner())
+  .pipe(jasmineBrowser.server({ port: 8080 }));
+
+}
+
+exports.test = test
+
+/*---------------------------------------------------------------*/
 //err
 
 // const gulp = require("gulp");
